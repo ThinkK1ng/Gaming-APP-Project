ec6773de2da831988a49225d43e6086d9be4276b
CASTICRI003 <ccastillo0924@gmail.com>
2022-11-17 19:54:15 -0600

    added our other html and js files. renamed original js file to homepage

---
+++ assets/js/homepage.js
@@ -0,0 +1,19 @@
+
+
+const gamePricesAPI = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4791ab9712msh7147de2993ea896p10b583jsn6d99266260b6',
+		'X-RapidAPI-Host': 'game-prices.p.rapidapi.com'
+	}
+};
+
+const twitchPopAPI = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '4791ab9712msh7147de2993ea896p10b583jsn6d99266260b6',
+            'X-RapidAPI-Host': 'twitch-game-popularity.p.rapidapi.com'
+        }
+    };
+    
+
