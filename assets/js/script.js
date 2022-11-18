33f07626e1f1b65d790546f8ecc8dba9d3df8f4a
CASTICRI003 <ccastillo0924@gmail.com>
2022-11-17 19:25:00 -0600

    added Twitch Popularity API and fixed Game Prices API code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const options = {
+const gamePricesAPI = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '4791ab9712msh7147de2993ea896p10b583jsn6d99266260b6',
@@ -6,7 +6,21 @@ const options = {
 	}
 };
 
-fetch('https://game-prices.p.rapidapi.com/game/minecraft?region=us&type=game', options)
+fetch('https://game-prices.p.rapidapi.com/games?title=&region=us&limit=10', gamePricesAPI)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+
+
+    const twitchPopAPI = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '4791ab9712msh7147de2993ea896p10b583jsn6d99266260b6',
+            'X-RapidAPI-Host': 'twitch-game-popularity.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://twitch-game-popularity.p.rapidapi.com/games', twitchPopAPI)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
